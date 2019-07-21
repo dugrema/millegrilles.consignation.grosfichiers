62f4251ad39695f91fd95eb1202a8ced69db5ca8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-20 20:18:05 -0400

    Ajout https, travail sur proxy.

---
+++ routes/grosfichiers.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var router = express.Router();
+
+
+// Router pour fichiers locaux (meme MilleGrille)
+var localRouter = express.Router();
+router.use('/local', localRouter);
+localRouter.get('*', processFichiersLocaux);
+localRouter.post('*', processFichiersLocaux);
+
+function processFichiersLocaux(req, res) {
+  console.log("ProcessFichiersLocaux methode:" + req.method + ": " + req.url);
+
+  // Le serveur supporte une requete GET ou POST pour aller chercher les fichiers
+  // GET devrait surtout etre utilise pour le developpement
+  let fuuide = req.body.fuuide || req.url;
+  var fileName = req.body.nomfichier;
+  var contentType = req.body.contenttype || 'application/octet-stream';
+
+  var filePath = path.join('/home/mathieu/work/downloadStaging/local', fuuide);
+  var stat = fs.statSync(filePath);
+
+  let header = {
+    'Content-Type': contentType,
+    'Content-Length': stat.size,
+  }
+  if(fileName) {
+    header['Content-Disposition'] = 'filename="' + fileName + '"';
+  }
+
+  res.writeHead(200, header);
+  var readStream = fs.createReadStream(filePath);
+
+  readStream.pipe(res);
+};
+
+router.put('/nouveauFichier', function(req, res, next) {
+
+});
+
+router.put('/nouvelleVersion', function(req, res, next) {
+
+});
+
+// Router pour fichiers tiers (autres MilleGrilles)
+// var tiersRouter = express.Router();
+// router.use('/tiers', tiersRouter);
+
+module.exports = router;
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CERT_FOLDER=/home/mathieu/certificates
+
+# export COUPDOEIL_SESSION_TIMEOUT=15000
+export MG_MQ_CAFILE=$CERT_FOLDER/millegrilles/pki.millegrilles.ssl.CAchain
+export MG_MQ_CERTFILE=$CERT_FOLDER/millegrilles/certs/dev2.maple.mdugre.info.cert.pem
+export MG_MQ_KEYFILE=$CERT_FOLDER/millegrilles/privkeys/dev2.maple.mdugre.info.pem
+export CERT=$CERT_FOLDER/dev2.maple.mdugre.info/fullchain.pem
+export PRIVKEY=$CERT_FOLDER/dev2.maple.mdugre.info/privkey.pem
+export MG_MQ_URL=amqps://dev2:5673/dev2
+export PORT=3003
+
+export MG_HTTPPROXY_SECURE=false
+
+npm start
+
