7eb3363f52e94faf0e77a67d2c1a726a3ce89439
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-20 15:43:36 -0400

    Update script demarrage, certs.

--- start.sh
+++ start.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-CERT_FOLDER=/home/mathieu/certificates
+CERT_FOLDER=/opt/millegrilles/dev2/pki
 export MG_NOM_MILLEGRILLE=dev2
 
 # export COUPDOEIL_SESSION_TIMEOUT=15000
-export MG_MQ_CAFILE=$CERT_FOLDER/millegrilles/certs/millegrille_dev2.maple.mdugre.info.cert.pem.rootchain
-export MG_MQ_CERTFILE=$CERT_FOLDER/millegrilles/certs/dev2.maple.mdugre.info.cert.pem.20190808
-export MG_MQ_KEYFILE=$CERT_FOLDER/millegrilles/privkeys/dev2.maple.mdugre.info.pem.20190808
-export MG_MQ_URL=amqps://mq:5673/$MG_NOM_MILLEGRILLE
-export PORT=3003
+export MG_MQ_URL=amqps://dev2.local:5673/$MG_NOM_MILLEGRILLE
 
-# export CERT=$CERT_FOLDER/dev2.maple.mdugre.info/fullchain.pem
-# export PRIVKEY=$CERT_FOLDER/dev2.maple.mdugre.info/privkey.pem
-export CERT=$MG_MQ_CERTFILE
-export PRIVKEY=$MG_MQ_KEYFILE
+export MG_MQ_CAFILE=$CERT_FOLDER/certs/dev2_CA_chain.cert.pem
+export MG_MQ_CERTFILE=$CERT_FOLDER/certs/dev2_middleware.cert.pem
+export MG_MQ_KEYFILE=$CERT_FOLDER/keys/dev2_middleware.key.pem
+
+export CERT=$CERT_FOLDER/certs/dev2_fullchain.cert.pem
+export PRIVKEY=$CERT_FOLDER/keys/dev2_middleware.key.pem
+export PORT=3003
 
 export MG_HTTPPROXY_SECURE=false
 
