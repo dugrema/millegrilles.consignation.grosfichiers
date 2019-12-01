b171edd54fd0ff7797f591dc10ff1fd8840923d1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-01 23:34:41 +0000

    Creation hard links.

--- messages/torrent.js
+++ messages/torrent.js
@@ -62,30 +62,51 @@ class TorrentMessages {
   _creerRepertoireHardlinks(message) {
     console.debug("Creer repertoire hard links");
 
-    // Creer repertoire pour collection figee
-    const nomCollection = message.nom;
-    const pathCollection = pathConsignation.formatPathTorrentCollection(nomCollection);
-
     return new Promise((resolve, reject) => {
+
+      // Creer repertoire pour collection figee
+      const nomCollection = message.nom + '.' + message.uuid;
+      const pathCollection = pathConsignation.formatPathTorrentCollection(nomCollection);
+
       fs.mkdir(pathCollection, e=>{
         if(e) {
           reject(e);
           return;
         }
 
-        const documents = message.documents;
-        for(let idx in documents) {
-          let fileDoc = documents[idx];
+        // Boucler sur les documents
+        var idx = 0;
+        const linkDocsLoop = function(documents) {
+          let fileDoc = documents[idx++];
 
           let fuuid = fileDoc.fuuid;
           let securite = fileDoc.securite;
-          let encrypte = securite == '3.protege' || securite == '4.protege';
+          let encrypte = securite == '3.protege' || securite == '4.secure';
           const pathFichier = pathConsignation.trouverPathLocal(fuuid, encrypte);
           console.debug("Creer hard link pour " + pathFichier);
 
-        }
+          // Nom du fichier:
+          // Si 1.public ou 2.prive: nomFichier (extension deja inclue)
+          // Si 3.protege: nomFichier.mgs1 (2e extension)
+          // Si 4.secure: fuuid.mgs1
+          const nomFichier = ((securite!='4.secure')?fileDoc.nom+'.':'') + fileDoc.fuuid + (encrypte?'.mgs1':'');
+          const newPathFichier = path.join(pathCollection, nomFichier);
+          fs.link(pathFichier, newPathFichier, e=>{
+            if(e) {
+              reject(e);
+              return;
+            }
 
-        resolve();
+            if(documents.length < idx) {
+              // Continuer boucle
+              linkDocsLoop();
+            } else{
+              resolve();
+            }
+          })
+        };
+        linkDocsLoop(message.documents); // Demarrer boucle
+
       })
     });
 
