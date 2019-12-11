7cfd139f555f5d5ac6bd77c121eb5c2c240931f1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-11 22:05:12 +0000

    Changement certs.

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-CERT_FOLDER=/opt/millegrilles/dev3/pki/deployeur
+CERT_FOLDER=/home/mathieu/mgdev/certs
 export MG_NOM_MILLEGRILLE=dev3
 
 # export COUPDOEIL_SESSION_TIMEOUT=15000
-export MG_MQ_CAFILE=$CERT_FOLDER/pki.ca.fullchain.pem
-export MG_MQ_CERTFILE=$CERT_FOLDER/deployeur.cert.pem
-export MG_MQ_KEYFILE=$CERT_FOLDER/deployeur.key.pem
+export MG_MQ_CAFILE=$CERT_FOLDER/pki.ca.root.cert
+export MG_MQ_CERTFILE=$CERT_FOLDER/pki.fichiers.fullchain
+export MG_MQ_KEYFILE=$CERT_FOLDER/pki.fichiers.key
 export CERT=$MG_MQ_CERTFILE
 export PRIVKEY=$MG_MQ_KEYFILE
 export MG_MQ_URL=amqps://mg-dev3.local:5673/$MG_NOM_MILLEGRILLE
