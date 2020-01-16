94c71d684a35169e73e8c85ed05fc0c73197be33
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-01-16 23:33:56 +0000

    Ajout reponse cert sur demande

--- messages/pki.js
+++ messages/pki.js
@@ -14,24 +14,46 @@ class PkiMessages {
   }
 
   enregistrerChannel() {
-    this.fingerprint = this.transmettreCertificat();
-    this.routingKeys = ['pki.requete.' + this.fingerprint]
+    this.fingerprint = transmettreCertificat(this.mq);
+    this.routingKeys = [
+      'pki.requete.' + this.fingerprint,
+      'pki.requete.role.fichiers',
+    ]
 
-    this.mq.routingKeyManager.addRoutingKeyCallback(this.transmettreCertificat, this.routingKeys);
+    const mq = this.mq;
+    this.mq.routingKeyManager.addRoutingKeyCallback(
+      function(routingKeys, message, opts) {transmettreCertificat(mq, routingKeys, message, opts)},
+      this.routingKeys
+    );
   }
 
-  transmettreCertificat() {
-    let messageCertificat = pki.preparerMessageCertificat();
-    let fingerprint = messageCertificat.fingerprint;
+}
+
+function transmettreCertificat(mq, routingKeys, message, opts) {
+  // console.debug("transmettreCertificat");
+  // console.debug(routingKeys);
+  // console.debug(message);
+  // console.debug(opts);
+
+  var replyTo, correlationId;
+  if(opts && opts.properties) {
+    replyTo = opts.properties.replyTo;
+    correlationId = opts.properties.correlationId;
+  }
+
+  let messageCertificat = pki.preparerMessageCertificat();
+  let fingerprint = messageCertificat.fingerprint;
+
+  if(replyTo && correlationId) {
+    mq.transmettreReponse(messageCertificat, replyTo, correlationId);
+  } else {
     let messageJSONStr = JSON.stringify(messageCertificat);
-    this.mq._publish(
+    mq._publish(
       'pki.certificat.' + fingerprint, messageJSONStr
     );
-
-    return fingerprint;
   }
 
-
+  return fingerprint;
 }
 
 module.exports = {PkiMessages};
