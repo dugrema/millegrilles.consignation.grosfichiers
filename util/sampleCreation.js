db22eacb664412a624f5abb97ef2ad01af92e8ca
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-01-29 14:20:43 +0000

    Creation sample annuel
    Ajout samples1-5, avec 5 qui represente un backup de sous-domaine avec 
    tous les types d'archives

--- util/sampleCreation.js
+++ util/sampleCreation.js
@@ -108,13 +108,12 @@ async function creerBackupQuotidien(dateJour, opts) {
   const fichiersArchiveQuotidienne = []
 
   // Preparer des backup horaires a mettre dans le .tar
-  var promises = []
-  promises.push(creerBackupHoraire(new Date(`${dateJourFormatte} 03:00`), {rep}))
-  promises.push(creerBackupHoraire(new Date(`${dateJourFormatte} 06:00`), {rep}))
-  promises.push(creerBackupHoraire(new Date(`${dateJourFormatte} 07:00`), {rep}))
-  const cataloguesHoraire = await Promise.all(promises)
+  var cataloguesHoraire = []
+  cataloguesHoraire.push(await creerBackupHoraire(new Date(`${dateJourFormatte} 03:00`), {rep}))
+  cataloguesHoraire.push(await creerBackupHoraire(new Date(`${dateJourFormatte} 06:00`), {rep}))
+  cataloguesHoraire.push(await creerBackupHoraire(new Date(`${dateJourFormatte} 07:00`), {rep}))
 
-  console.debug("Promises! : %O", cataloguesHoraire)
+  console.debug("Promises catalogue quotidien - creation horaire : %O", cataloguesHoraire)
 
   const fichiers = {}
   for(idx in cataloguesHoraire) {
@@ -146,38 +145,130 @@ async function creerBackupQuotidien(dateJour, opts) {
 
   const pathBase = path.join(PATH_BACKUP, rep, 'horaire')
   const nomArchive = `domaine.test_${dateFormattee}.tar`
-  const pathArchive = path.join(PATH_BACKUP, nomArchive)
+  const pathArchive = path.join(PATH_BACKUP, rep, nomArchive)
 
   // Creer archive tar quotidienne
   // Creer nouvelle archive annuelle
+  console.debug("Creer archive quotidienne: %s", pathArchive)
   await tar.c(
     {
       file: pathArchive,
       cwd: pathBase,
     },
     fichiersArchiveQuotidienne
-  )
+  ).then(()=>{
+    console.debug("TAR cree : %s", pathArchive)
+  })
 
   // Supprimer tous les fichiers dans l'archive
   for(let idx in fichiersArchiveQuotidienne) {
     const fichier = path.join(PATH_BACKUP, rep, 'horaire', fichiersArchiveQuotidienne[idx])
     fs.unlinkSync(fichier)
   }
+  fs.rmdirSync(path.join(PATH_BACKUP, rep, 'horaire'))
 
+  return {pathArchive, catalogue}
+}
+
+async function creerBackupAnnuel(dateAnnee, opts) {
+  opts = opts || {}
+  var rep = opts.rep || ''
+
+  const anneeFormattee = formatterDateString(dateAnnee).slice(0, 4)
+
+  const archivesQuotidiennes = []
+  archivesQuotidiennes.push(await creerBackupQuotidien(new Date(`${anneeFormattee}-01-01`), {rep}))
+  archivesQuotidiennes.push(await creerBackupQuotidien(new Date(`${anneeFormattee}-01-02`), {rep}))
+  archivesQuotidiennes.push(await creerBackupQuotidien(new Date(`${anneeFormattee}-01-04`), {rep}))
+  archivesQuotidiennes.push(await creerBackupQuotidien(new Date(`${anneeFormattee}-02-05`), {rep}))
+  archivesQuotidiennes.push(await creerBackupQuotidien(new Date(`${anneeFormattee}-03-06`), {rep}))
+
+  console.debug("Backup annuel, archives quotidiennes crees: %O", archivesQuotidiennes)
+
+  const fichiers = {}, fichiersArchiveAnnuelle = []
+  for(idx in archivesQuotidiennes) {
+    const {pathArchive, catalogue} = archivesQuotidiennes[idx]
+
+    // const pathArchiveComplet = path.join(PATH_BACKUP, rep, pathArchive)
+
+    fichiersArchiveAnnuelle.push(path.basename(pathArchive))
+
+    const hachageArchive = await calculerHachageFichier(pathArchive)
+    var jour = formatterDateString(new Date(catalogue.jour*1000)).slice(0,8)
+    fichiers[jour] = {
+      archive_nomfichier: path.basename(pathArchive),
+      archive_hachage: hachageArchive,
+    }
+  }
+
+  const catalogue = {
+    fichiers_quotidiens: fichiers,
+    annee: dateAnnee.getTime()/1000,
+  }
+
+  const pathCatalogue = `${rep}/domaine.test_${anneeFormattee}.json.xz`
+  await sauvegarderFichier(pathCatalogue, catalogue, {lzma: true})
+  fichiersArchiveAnnuelle.unshift(path.basename(pathCatalogue))
+
+  const pathBase = path.join(PATH_BACKUP, rep)
+  const nomArchive = `domaine.test_${anneeFormattee}.tar`
+  const pathArchive = path.join(PATH_BACKUP, rep, nomArchive)
+
+  // Creer archive tar quotidienne
+  // Creer nouvelle archive annuelle
+  console.debug("Creer archive annuelle: %s\n%O", pathArchive, fichiersArchiveAnnuelle)
+  await tar.c(
+    {
+      file: pathArchive,
+      cwd: pathBase,
+    },
+    fichiersArchiveAnnuelle
+  ).then(()=>{
+    console.debug("TAR cree : %s", pathArchive)
+  })
+
+  // Supprimer tous les fichiers dans l'archive
+  for(let idx in fichiersArchiveAnnuelle) {
+    const fichier = path.join(PATH_BACKUP, rep, fichiersArchiveAnnuelle[idx])
+    fs.unlinkSync(fichier)
+  }
+
+  return pathArchive
 }
 
 async function creerSamples() {
   fs.mkdirSync(PATH_BACKUP)
 
-  var promises = []
-  promises.push(creerBackupHoraire(new Date("2020-01-01 00:00"), {rep: 'sample1'}))
-  promises.push(creerBackupHoraire(new Date("2020-01-01 02:00"), {rep: 'sample1'}))
-  promises.push(creerBackupHoraire(new Date("2020-01-01 03:00"), {rep: 'sample1'}))
-  promises.push(creerBackupHoraire(new Date("2020-01-01 04:00"), {rep: 'sample1'}))
+  // Fichiers horaires
+  await creerBackupHoraire(new Date("2020-01-01 00:00"), {rep: 'sample1'})
+  await creerBackupHoraire(new Date("2020-01-01 02:00"), {rep: 'sample1'})
+  await creerBackupHoraire(new Date("2020-01-01 03:00"), {rep: 'sample1'})
+  await creerBackupHoraire(new Date("2020-01-01 04:00"), {rep: 'sample1'})
 
-  promises.push(creerBackupQuotidien(new Date("2020-02-01"), {rep: 'sample2'}))
+  // Archive quotidienne
+  await creerBackupQuotidien(new Date("2020-02-01"), {rep: 'sample2'})
 
-  await Promise.all(promises)
+  // Archives quotidiennes
+  await creerBackupQuotidien(new Date("2020-03-01"), {rep: 'sample3'})
+  await creerBackupQuotidien(new Date("2020-04-02"), {rep: 'sample3'})
+  await creerBackupQuotidien(new Date("2020-03-03"), {rep: 'sample3'})
+
+  // Archive annuelle
+  await creerBackupAnnuel(new Date("2020-01-01"), {rep: 'sample4'})
+
+  // Sample 5 - tous les types d'archive pour tester de "walking"
+  await creerBackupAnnuel(new Date("2018-01-01"), {rep: 'sample5'})
+  await creerBackupAnnuel(new Date("2019-01-01"), {rep: 'sample5'})
+  await creerBackupQuotidien(new Date("2020-01-01"), {rep: 'sample5'})
+  await creerBackupQuotidien(new Date("2020-01-02"), {rep: 'sample5'})
+  await creerBackupQuotidien(new Date("2020-01-03"), {rep: 'sample5'})
+  await creerBackupQuotidien(new Date("2020-01-04"), {rep: 'sample5'})
+  await creerBackupHoraire(new Date("2020-01-05 00:00"), {rep: 'sample5'})
+  await creerBackupHoraire(new Date("2020-01-05 01:00"), {rep: 'sample5'})
+  await creerBackupHoraire(new Date("2020-01-05 02:00"), {rep: 'sample5'})
+  await creerBackupHoraire(new Date("2020-01-05 03:00"), {rep: 'sample5'})
+
+  //await Promise.all(promises)
 }
 
 module.exports = creerSamples
