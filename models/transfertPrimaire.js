616baaf65f82e45c9ced1089be450ad50dcf36dd
Mathieu Dugre <mathieu.dugre@mdugre.info>
2023-02-08 12:19:44 -0500

    Transfert secondaire vers primaire
    Complete pour local

--- models/transfertPrimaire.js
+++ models/transfertPrimaire.js
@@ -65,52 +65,7 @@ TransfertPrimaire.prototype.putFichier = async function(fuuid) {
         return
     }
 
-    // const pathReadyItem = path.join(pathReady, item)
-    // debug("Traiter transfert vers consignation de %s", pathReadyItem)
-
-    // let etat = null
-    // {
-    //     // Charger etat et maj dates/retry count
-    //     const pathEtat = path.join(pathReadyItem, FICHIER_ETAT)
-    //     etat = JSON.parse(await fsPromises.readFile(pathEtat))
-    //     etat.lastProcessed = new Date().getTime()
-    //     etat.retryCount++
-    //     await fsPromises.writeFile(pathEtat, JSON.stringify(etat), {mode: 0o600})
-    // }
-    // const hachage = etat.hachage
-
-    // // Verifier les transactions (signature)
-    // const transactions = await traiterTransactions(amqpdao, pathReady, item)
-
-    // // Verifier le fichier (hachage)
-    // await verifierFichier(hachage, pathReadyItem)
-
-    // debug("Transactions et fichier OK : %s", pathReadyItem)
-
-    // const promiseReaddirp = readdirp(pathReadyItem, {
-    //     type: 'files',
-    //     fileFilter: '*.part',
-    //     depth: 1,
-    // })
-
-    // const listeFichiersTriees = []
-    // for await (const entry of promiseReaddirp) {
-    //     // debug("Entry path : %O", entry);
-    //     const fichierPart = entry.basename
-    //     const position = Number(fichierPart.split('.').shift())
-    //     listeFichiersTriees.push({...entry, position})
-    // }
-
-    // // Trier par position (asc)
-    // listeFichiersTriees.sort((a,b)=>a.position-b.position)
-
     debug("Traiter PUT pour fuuid %s", fuuid)
-    // if(positionUpload > position) {
-    //     debug("Skip position %d (courante connue est %d)", position, positionUpload)
-    //     continue
-    // } else {
-    //     positionUpload = position
-    // }
 
     try {
         await this.putAxios(fuuid, statItem)
@@ -124,16 +79,6 @@ TransfertPrimaire.prototype.putFichier = async function(fuuid) {
             throw err
         }
     }
-
-    // // Faire POST pour confirmer upload, acheminer transactions
-    // const urlPost = new URL(''+_urlConsignationTransfert)
-    // urlPost.pathname = path.join(urlPost.pathname, item)
-    // const reponsePost = await axios({
-    //     method: 'POST',
-    //     httpsAgent: _httpsAgent,
-    //     url: urlPost.href,
-    //     data: transactions,
-    // })
     
 }
 
@@ -146,6 +91,23 @@ TransfertPrimaire.prototype.putAxios = async function(fuuid, statItem) {
           httpsAgent = this.storeConsignation.getHttpsAgent(),
           urlConsignationTransfert = this.storeConsignation.getUrlTransfert()
 
+    // S'assurer que le fichier n'existe pas deja
+    try {
+        const urlFuuid = new URL(urlConsignationTransfert.href)
+        urlFuuid.pathname = path.join(urlFuuid.pathname, fuuid)
+        await axios({method: 'HEAD', url: urlFuuid.href, httpsAgent})
+        console.error(new Date() + "transfertPrimaire.putAxios Fichier %s existe deja sur le primaire, annuler transfert", fuuid)
+        return false
+    } catch(err) {
+        const response = err.response
+        if(response && response.status === 404) {
+            // OK, le fuuid n'existe pas deja
+        } else {
+            debug("Erreur axios : ", err)
+            throw err
+        }
+    }
+
     for (let position=0; position < size; position += CONST_TAILLE_SPLIT_MAX_DEFAULT) {
         // Creer output stream
         const start = position,
@@ -157,67 +119,28 @@ TransfertPrimaire.prototype.putAxios = async function(fuuid, statItem) {
 
         const readStream = fs.createReadStream(filePath, {start, end})
         try {
-            const reponsePut = await axios({
+            await axios({
                 method: 'PUT',
                 httpsAgent,
                 url: urlPosition.href,
-                headers: {'content-type': 'application/stream'},
+                headers: {'content-type': 'application/stream', 'X-fuuid': fuuid},
                 data: readStream,
             })
-                    
         } finally {
             readStream.close()
         }
     }
     
-
-    // if(typeof(inputStream._read === 'function')) {
-    //     // Assumer stream
-    //     let compteur = 0
-    //     const promise = new Promise((resolve, reject)=>{
-    //         inputStream.on('data', async chunk => {
-    //             inputStream.pause()
-                
-    //             if(chunk.length + compteur > TAILLE_SPLIT) {
-    //                 debug("stagingStream split %s", pathFichierPut)
-    //                 const bytesComplete = TAILLE_SPLIT - compteur
-    //                 await writer.write(chunk.slice(0, bytesComplete))  // Ecrire partie du chunk
-    //                 await writer.close()  // Fermer le fichier
-
-    //                 pathFichierPut = await getPathRecevoir(pathStaging, correlation, position + bytesComplete)
-    //                 debug("stagingStream Ouvrir nouveau fichier %s", pathFichierPut)
-    //                 writer = await fsPromises.open(pathFichierPut, 'w')
-
-    //                 position += chunk.length       // Aller a la fin du chunk
-    //                 compteur = chunk.length - bytesComplete   // Reset compteur
-    //                 await writer.write(chunk.slice(bytesComplete))  // Continuer a la suite dans le chunk
-    //             } else {
-    //                 debug("stagingStream chunk %d dans %s", chunk.length, pathFichierPut)
-    //                 compteur += chunk.length
-    //                 position += chunk.length
-    //                 await writer.write(chunk)
-    //             }
-                
-    //             inputStream.resume()
-    //         })
-    //         inputStream.on('end', async () => { 
-    //             await writer.close()
-    //             resolve()
-    //         })
-    //         inputStream.on('error', err=>{ reject(err) })
-    //     })
-    //     //inputStream.pipe(writer)
-    //     inputStream.read()  // Lancer la lecture
-        
-    //     return promise
-    // } else {
-    //     throw new Error("Type input non supporte")
-    // }    
+    // Emettre message POST pour indiquer que le fichier est complete
+    const transactions = { etat: { hachage: fuuid } }
+    const urlCorrelation = new URL(urlConsignationTransfert.href)
+    urlCorrelation.pathname = path.join(urlCorrelation.pathname, correlation)
+    await axios({
+        method: 'POST',
+        httpsAgent,
+        url: urlCorrelation.href,
+        data: transactions,
+    })
 }
-
-// const fichierActifsPrimaireDest = path.join(getPathDataFolder(), 'actifsPrimaire.txt')
-
-// const httpsAgent = FichiersTransfertBackingStore.getHttpsAgent()
-// const urlTransfert = new URL(FichiersTransfertBackingStore.getUrlTransfert())
 
 module.exports = TransfertPrimaire
