42cf1546b46c0d8d36fb8526a8cb989a117db8fb
Mathieu Dugre <mathieu.dugre@mdugre.info>
2023-02-16 16:32:31 -0500

    Bugfix intervalle refresh, detect fichier missing

--- models/storeConsignation.js
+++ models/storeConsignation.js
@@ -78,7 +78,7 @@ async function changerStoreConsignation(typeStore, params, opts) {
         debug("changerStoreConsignation Set sync actif ", _syncActif)
     }
     if(params.sync_intervalle !== undefined) {
-        _intervalleSync = params.sync_intervalle
+        _intervalleSync = params.sync_intervalle * 1000  // Convertir de secondes en millisecs
         debug("changerStoreConsignation Set sync intervalle %d secs", _intervalleSync)
     }
 
@@ -576,8 +576,9 @@ async function downloadFichiersSync() {
     const rlFichiers = readline.createInterface({input: readStreamFichiers, crlfDelay: Infinity})
     for await (const line of rlFichiers) {
         // Detecter fichiers manquants localement par espaces vide au debut de la ligne
-        if(line.startsWith('	')) {
+        if( line.startsWith(' ') || line.startsWith('\t') ) {
             const fuuid = line.trim()
+            debug('downloadFichiersSync ajouter ', fuuid)
             _queueDownloadFuuids.add(fuuid)
         }
     }
@@ -701,7 +702,7 @@ async function uploaderFichiersVersPrimaire() {
         const rlFichiers = readline.createInterface({input: readStreamFichiers, crlfDelay: Infinity})
         for await (const line of rlFichiers) {
             // Detecter changement distant avec un fuuid dans la premiere colonne du fichier (pas d'espaces vides)
-            if( ! line.startsWith(' ')) {
+            if( ! line.startsWith(' ') && ! line.startsWith('\t') ) {
                 const fuuid = line.trim()
                 debug("uploaderFichiersVersPrimaire Transferer fichier manquant %s vers primaire", fuuid)
                 _transfertPrimaire.ajouterItem(fuuid)
